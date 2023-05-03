f635eb23ee296b89c139c6427ba6d14d86aef641
Jason Stewart <jaysin.stewart@gmail.com>
2023-05-03 16:38:32 -0700

    Update index.js render band name fucntion

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code jovascript here
+
+// Render to DOM Functions
+
+function renderBandNameCard (band) {
+    let bandCard = document.createElement("div")
+    bandCard.classList.add("band-name-element")
+    
+    let h2 = document.createElement("h2")
+    h2.textContent = band.name
+}
