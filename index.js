1387ba7d4b347322af231ef008ab92b88de3abe7
Jason Stewart <jaysin.stewart@gmail.com>
2023-05-03 16:52:35 -0700

    Update index.js intial render get band names

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 
 // Render to DOM Functions
 
-function renderBandNameCard (band) {
-    let bandCard = document.createElement("div")
-    bandCard.classList.add("band-name-element")
+function renderBandNamesDisplay (band) {
+    let bandName = document.createElement("div")
+    bandName.classList.add("band-name-element")
     
     let h2 = document.createElement("h2")
     h2.textContent = band.name
+
+    document.getElementsByClassName("band-names-list").appendChild(bandName)
 }
+
+// Fetches
+
+function getAllBandNames () {
+    fetch("http://localhost:3000/bands")
+        .then(res => res.json())
+        .then(bandNames => bandNames.forEach(renderBandNamesDisplay))
+}
+
+// Initial Render
+function intialize () {
+    getAllBandNames()
+}
+intialize()
