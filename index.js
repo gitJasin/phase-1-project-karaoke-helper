fda1e465465377e3d40aeb7f30e493bc1f871ae2
Jason Stewart <jaysin.stewart@gmail.com>
2023-05-03 17:26:35 -0700

    Update index.js band names not rendering

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 
 // Render to DOM Functions
-
-function renderBandNamesDisplay (band) {
+function renderBandNames (band) {
     let bandName = document.createElement("div")
     bandName.classList.add("band-name-element")
     
     let h2 = document.createElement("h2")
     h2.textContent = band.name
 
-    document.getElementsByClassName("band-names-list").appendChild(bandName)
+    document.querySelector(".band-names-list").appendChild(bandName)
+    document.querySelector(".band-name-element").append(h2)
 }
 
 // Fetches
-
 function getAllBandNames () {
     fetch("http://localhost:3000/bands")
         .then(res => res.json())
-        .then(bandNames => bandNames.forEach(renderBandNamesDisplay))
+        .then(bands => bands.forEach(band => renderBandNames(band)))
 }
 
 // Initial Render
