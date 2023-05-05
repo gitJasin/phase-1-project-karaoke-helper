98627d1982265d3e6659242faa8f39fc4a57a893
Jason Stewart <jaysin.stewart@gmail.com>
2023-05-05 13:27:56 -0700

    Add song list

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ function renderBandNames (band) {
     document.querySelector(".band-scroller").append(bandName)
 }
 
+function renderSongNames (song) {
+    let songName = document.createElement("div")
+    songName.classList.add("song-name-element")
+
+    let h3 = document.createElement("h3")
+    h3.textContent = song.song
+
+    songName.appendChild(h3)
+    document.querySelector(".song-scroller").append(songName)
+}
 // Fetches
 function getAllBandNames () {
     fetch("http://localhost:3000/songs")
@@ -18,8 +28,14 @@ function getAllBandNames () {
         .then(songs => songs.forEach(band => renderBandNames(band)))
 }
 
+function getAllSongs () {
+    fetch("http://localhost:3000/songs")
+    .then(res => res.json())
+    .then(songs => songs.forEach(song => renderSongNames(song)))
+}
 // Initial Render
 function intialize () {
     getAllBandNames()
+    getAllSongs()
 }
 intialize()
